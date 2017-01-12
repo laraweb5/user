583c640ba6fb004cd9190058ea4cfa2e1ec8719e
honjyo <honjyo@linkstaff.co.jp>
2017-01-12 10:56:41 +0900

    README修正

--- app/User.php
+++ app/User.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+     protected $table = 'user_admin';
+
     //挿入できるようにカラムを宣言
    # protected $fillable = ['name','mail','gender','age','pref','birthday','tel'];
 }
